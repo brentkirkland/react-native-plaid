3bf3a8a927c0b45bcbb3d9b1bf33ab50fc72a5c8
Brent Kirkland <brentckirkland@gmail.com>
2015-06-10 14:00:50 -0700

    basic BofA login frame up

--- index.ios.js
+++ index.ios.js
@@ -3,11 +3,12 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
-
+var Button = require('react-native-button');
 var React = require('react-native');
 var {
   AppRegistry,
   StyleSheet,
+  TextInput,
   Text,
   View,
 } = React;
@@ -17,15 +18,20 @@ var MoneyLover = React.createClass({
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+           {' Bank of America '}
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <TextInput 
+          style={styles.textField} placeholder='username' autoCorrect='No'
+          onChangeText={(text) => this.setState({input: text})}
+        />
+        <TextInput
+          style={styles.passwordTextField} placeholder='password' secureTextEntry='YES'
+          onChangeText={(text) => this.setState({input: text})}
+        />
+        <Button 
+          style={styles.submit} onPress={this._handlePress}>
+          {' Submit '}
+        </Button>
       </View>
     );
   }
@@ -35,13 +41,48 @@ var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#FF4A4A',
+  },
+  textField: {
+    height: 40, 
+    marginLeft: 20, 
+    marginRight: 20, 
+    paddingLeft: 5, 
+    backgroundColor: '#FFF', 
+    color: '#FF4A4A'
+  },
+  passwordTextField: {
+    height: 40, 
+    marginTop: 5,
+    marginLeft: 20, 
+    marginRight: 20, 
+    paddingLeft: 5, 
+    backgroundColor: '#FFF', 
+    color: '#FF4A4A'
+  },
+  seperator: {
+    height: 1,
+    width: 100,
+    backgroundColor: '#FFFFFF',
   },
   welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginLeft: 20,
+    marginBottom: 5,
+    backgroundColor: '#FFF',
+    color: "#FF4A4A",
+  },
+  submit: {
+    padding: 9,
+    fontSize: 17,
+    marginTop: 5,
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 5,
+    borderWidth: 1,
+    borderColor: 'white',
+    color: "#FFF",
   },
   instructions: {
     textAlign: 'center',
