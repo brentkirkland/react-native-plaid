462d7f2710ca0bbc5339e06d96ea27336ebc1926
Brent Kirkland <brentckirkland@gmail.com>
2015-06-10 14:07:55 -0700

    moved login to module

---
+++ App/components/BankLogin.js
@@ -0,0 +1,91 @@
+'use strict';
+
+var Button = require('react-native-button');
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  TextInput,
+  Text,
+  View,
+} = React;
+
+var BankLogin = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+           {' Bank of America '}
+        </Text>
+        <TextInput 
+          style={styles.textField} placeholder='username' autoCorrect='No'
+          onChangeText={(text) => this.setState({input: text})}
+        />
+        <TextInput
+          style={styles.passwordTextField} placeholder='password' secureTextEntry='YES'
+          onChangeText={(text) => this.setState({input: text})}
+        />
+        <Button 
+          style={styles.submit} onPress={this._handlePress}>
+          {' Submit '}
+        </Button>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#FF4A4A',
+  },
+  textField: {
+    height: 40, 
+    marginLeft: 20, 
+    marginRight: 20, 
+    paddingLeft: 5, 
+    backgroundColor: '#FFF', 
+    color: '#FF4A4A'
+  },
+  passwordTextField: {
+    height: 40, 
+    marginTop: 5,
+    marginLeft: 20, 
+    marginRight: 20, 
+    paddingLeft: 5, 
+    backgroundColor: '#FFF', 
+    color: '#FF4A4A'
+  },
+  seperator: {
+    height: 1,
+    width: 100,
+    backgroundColor: '#FFFFFF',
+  },
+  welcome: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginLeft: 20,
+    marginBottom: 5,
+    backgroundColor: '#FFF',
+    color: "#FF4A4A",
+  },
+  submit: {
+    padding: 8.5,
+    fontSize: 17,
+    marginTop: 5,
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 5,
+    borderWidth: 1,
+    borderColor: 'white',
+    color: "#FFF",
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+module.exports = BankLogin;
--- index.ios.js
+++ index.ios.js
@@ -3,7 +3,7 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
-var Button = require('react-native-button');
+
 var React = require('react-native');
 var {
   AppRegistry,
@@ -13,25 +13,13 @@ var {
   View,
 } = React;
 
+var BankLogin = require('./App/components/BankLogin.js');
+
 var MoneyLover = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-           {' Bank of America '}
-        </Text>
-        <TextInput 
-          style={styles.textField} placeholder='username' autoCorrect='No'
-          onChangeText={(text) => this.setState({input: text})}
-        />
-        <TextInput
-          style={styles.passwordTextField} placeholder='password' secureTextEntry='YES'
-          onChangeText={(text) => this.setState({input: text})}
-        />
-        <Button 
-          style={styles.submit} onPress={this._handlePress}>
-          {' Submit '}
-        </Button>
+        <BankLogin/>
       </View>
     );
   }
@@ -42,52 +30,6 @@ var styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     backgroundColor: '#FF4A4A',
-  },
-  textField: {
-    height: 40, 
-    marginLeft: 20, 
-    marginRight: 20, 
-    paddingLeft: 5, 
-    backgroundColor: '#FFF', 
-    color: '#FF4A4A'
-  },
-  passwordTextField: {
-    height: 40, 
-    marginTop: 5,
-    marginLeft: 20, 
-    marginRight: 20, 
-    paddingLeft: 5, 
-    backgroundColor: '#FFF', 
-    color: '#FF4A4A'
-  },
-  seperator: {
-    height: 1,
-    width: 100,
-    backgroundColor: '#FFFFFF',
-  },
-  welcome: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginLeft: 20,
-    marginBottom: 5,
-    backgroundColor: '#FFF',
-    color: "#FF4A4A",
-  },
-  submit: {
-    padding: 9,
-    fontSize: 17,
-    marginTop: 5,
-    marginLeft: 20,
-    marginRight: 20,
-    marginBottom: 5,
-    borderWidth: 1,
-    borderColor: 'white',
-    color: "#FFF",
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
